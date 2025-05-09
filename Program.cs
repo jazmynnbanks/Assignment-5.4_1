87bbbd0e865aa5233f4dc926d55021b09fae2cc3
jazmy <jazmy@JAZ-LAPTOP>
2025-05-09 17:32:11 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿static void IndividualDigits(int x)
+{
+
+    if (x < 10)
+    {
+        Console.Write($"{x}" + " ");
+    }
+    else
+    {
+       IndividualDigits( x / 10);
+        Console.Write(( x % 10 ) + " ");
+
+    }
+}
+
+Console.Write("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+IndividualDigits(num);
